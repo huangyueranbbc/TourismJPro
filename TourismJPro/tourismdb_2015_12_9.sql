918a62a2577d3ba8ba451b2600242c883bae2d13
yyl <3081818283@qq.com>
2015-12-10 12:28:59 +0800

    第二次提交

--- TourismJPro/tourismdb_2015_12_9.sql
+++ TourismJPro/tourismdb_2015_12_9.sql
@@ -16,6 +16,23 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`tourismdb` /*!40100 DEFAULT CHARACTER S
 
 USE `tourismdb`;
 
+/*Table structure for table `t_bank` */
+
+DROP TABLE IF EXISTS `t_bank`;
+
+CREATE TABLE `t_bank` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `money` float(8,2) DEFAULT NULL,
+  `appid` varchar(100) DEFAULT '1001',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_bank` */
+
+insert  into `t_bank`(`id`,`name`,`password`,`money`,`appid`) values (1,'ruanko','ruanko',281375.00,'1001'),(2,'1','1',568625.00,'1001');
+
 /*Table structure for table `t_bill` */
 
 DROP TABLE IF EXISTS `t_bill`;
@@ -39,11 +56,13 @@ CREATE TABLE `t_bill` (
   PRIMARY KEY (`ID`),
   KEY `FK_user_bill` (`user_id`),
   KEY `FK_order_bill` (`order_id`),
-  CONSTRAINT `FK_order_bill` FOREIGN KEY (`order_id`) REFERENCES `t_order` (`id`),
-  CONSTRAINT `FK_user_bill` FOREIGN KEY (`user_id`) REFERENCES `t_user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT `FK_user_bill` FOREIGN KEY (`user_id`) REFERENCES `t_user` (`id`),
+  CONSTRAINT `FK_order_bill` FOREIGN KEY (`order_id`) REFERENCES `t_order` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 /*Data for the table `t_bill` */
+
+insert  into `t_bill`(`ID`,`user_id`,`order_id`,`NO`,`user_name`,`recv_account`,`bank`,`pay_account`,`currency`,`money`,`paymethod`,`type`,`status`,`remarks`,`del`) values (1,1,2,'2015-12-0911449648776717','1','ruanko','中国银行',525.00,0,525.00,'在线支付',0,3,'软库网旅游3:在线支付中国银行 支付金额:525.0',0),(2,1,2,'2015-12-0911449648776717','1','ruanko','中国银行',525.00,0,525.00,'在线支付',0,3,'软库网旅游3:在线支付中国银行 支付金额:525.0',0),(3,1,3,'2015-12-0911449648821406','1','ruanko','中国银行',11655.00,0,11655.00,'在线支付',0,3,'软库网旅游:在线支付中国银行 支付金额:11655.0',0),(4,1,4,'2015-12-0911449648830755','1','ruanko','中国银行',555.00,0,555.00,'在线支付',0,3,'软库网旅游3:在线支付中国银行 支付金额:555.0',0),(5,1,5,'2015-12-0911449648840337','1','ruanko','中国银行',218115.00,0,218115.00,'在线支付',0,3,'软库网旅游:在线支付中国银行 支付金额:218115.0',0);
 
 /*Table structure for table `t_comment` */
 
@@ -59,8 +78,8 @@ CREATE TABLE `t_comment` (
   `createDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `del` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
-  KEY `FK_user_comment` (`user_id`),
   KEY `FK_tourism_comment` (`toursim_id`),
+  KEY `FK_user_comment` (`user_id`),
   CONSTRAINT `FK_tourism_comment` FOREIGN KEY (`toursim_id`) REFERENCES `t_tourism` (`id`),
   CONSTRAINT `FK_user_comment` FOREIGN KEY (`user_id`) REFERENCES `t_user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -85,13 +104,15 @@ CREATE TABLE `t_order` (
   `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `del` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
-  KEY `FK_user_order` (`tourism_id`),
-  KEY `FK_order_tourism` (`user_id`),
-  CONSTRAINT `FK_order_tourism` FOREIGN KEY (`user_id`) REFERENCES `t_tourism` (`id`),
-  CONSTRAINT `FK_user_order` FOREIGN KEY (`tourism_id`) REFERENCES `t_user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  KEY `FK_user_order` (`user_id`),
+  KEY `FK_order_tourism` (`tourism_id`),
+  CONSTRAINT `FK_user_order` FOREIGN KEY (`user_id`) REFERENCES `t_user` (`id`),
+  CONSTRAINT `FK_order_tourism` FOREIGN KEY (`tourism_id`) REFERENCES `t_tourism` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 /*Data for the table `t_order` */
+
+insert  into `t_order`(`id`,`tourism_id`,`user_id`,`orderId`,`price`,`tourismTime`,`number`,`status`,`amount`,`remark`,`createTime`,`del`) values (1,5,1,'2015-12-0911449648683411',25.00,'2016-05-13 00:00:00',1,1,25.00,'软库网旅游3','2015-12-09 16:11:23',0),(2,5,1,'2015-12-0911449648776717',25.00,'2016-05-13 00:00:00',21,2,525.00,'软库网旅游3','2015-12-09 16:12:56',0),(3,1,1,'2015-12-0911449648821406',555.00,'2016-05-13 00:00:00',21,2,11655.00,'软库网旅游','2015-12-09 16:13:41',0),(4,4,1,'2015-12-0911449648830755',555.00,'2016-05-13 00:00:00',1,2,555.00,'软库网旅游3','2015-12-09 16:13:50',0),(5,1,1,'2015-12-0911449648840337',555.00,'2016-05-13 00:00:00',393,2,218115.00,'软库网旅游','2015-12-09 16:14:00',0);
 
 /*Table structure for table `t_orderprocess` */
 
@@ -109,9 +130,11 @@ CREATE TABLE `t_orderprocess` (
   KEY `FK_Reference_10` (`user_id`),
   CONSTRAINT `FK_order_orderProcess` FOREIGN KEY (`order_id`) REFERENCES `t_order` (`id`),
   CONSTRAINT `FK_Reference_10` FOREIGN KEY (`user_id`) REFERENCES `t_user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 /*Data for the table `t_orderprocess` */
+
+insert  into `t_orderprocess`(`id`,`user_id`,`order_id`,`type`,`dateTime`,`del`) values (1,1,1,1,'2015-12-09 16:11:23',0),(2,1,2,2,'2015-12-09 16:12:56',0),(3,1,3,2,'2015-12-09 16:13:41',0),(4,1,4,2,'2015-12-09 16:13:50',0),(5,1,5,2,'2015-12-09 16:14:00',0);
 
 /*Table structure for table `t_tourism` */
 
@@ -129,11 +152,11 @@ CREATE TABLE `t_tourism` (
   `description` text NOT NULL,
   `del` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 /*Data for the table `t_tourism` */
 
-insert  into `t_tourism`(`id`,`title`,`type`,`city`,`days`,`basePrice`,`minNumber`,`maxNumber`,`description`,`del`) values (21,'软库网旅游1',0,'深圳',80,2,1,888,'好玩啦',0),(22,'软库网旅游2',1,'深圳',80,2,1,888,'k可以玩',0),(23,'软库网旅游3',1,'深圳',80,2,1,888,'软库网',0),(24,'软库网旅游4',1,'深圳',80,2,1,888,'可以的',0),(25,'软库网旅游5',1,'深圳',80,2,1,888,'aa',0),(26,'软库网旅游6',1,'深圳',80,2,1,888,'d',0),(27,'软库网旅游7',1,'深圳',80,2,1,888,'a w',0),(28,'软库网旅游8',1,'深圳',80,2,1,888,'d',0),(29,'软库网旅游9',1,'深圳',80,2,1,888,'fsdf',0),(30,'软库网旅游10',0,'深圳',80,2,1,888,'好玩啦',0),(31,'软库网旅游11',0,'深圳',80,2,1,888,'好玩啦',0),(32,'软库网旅游12',1,'深圳',80,2,1,888,'aa',0),(33,'软库网旅游13',1,'深圳',80,2,1,888,'aa',0),(34,'软库网旅游14',1,'深圳',80,2,1,888,'可以的',0),(35,'软库网旅游15',1,'深圳',80,2,1,888,'可以的',0),(36,'软库网旅游16',1,'深圳',80,2,1,888,'软库网',0),(37,'软库网旅游18',1,'深圳',80,2,1,888,'可以的',0),(38,'软库网旅游19',1,'深圳',80,2,1,888,'可以的',0),(39,'软库网旅游20',1,'深圳',80,34,1,888,'软库网',0),(40,'软库网旅游22',1,'深圳',80,34645345,1,888,'可以的',0),(41,'软库网旅游23',1,'深圳',80,45,1,888,'软库网',0),(42,'软库网旅游25',1,'深圳',80,463,1,888,'k可以玩',0),(43,'软库网旅游26',1,'深圳',80,345,1,888,'软库网',0),(44,'软库网旅游27',1,'深圳',80,345,1,888,'软库网',0),(45,'软库网旅游28',1,'深圳',80,6,1,888,'软库网',0),(46,'软库网旅游29',1,'深圳',80,634,1,888,'软库网',0),(47,'软库网旅游30',1,'深圳',80,4234,1,888,'软库网',0),(48,'软库网旅游24',1,'深圳',80,235,1,888,'软库网',0),(49,'软库网旅游21',1,'深圳',80,6346,1,888,'软库网',0),(50,'软库网旅游17',1,'深圳',80,234,1,888,'软库网',0),(51,'软库网旅游',1,'深圳',80,534,1,888,'fewfwe ',0);
+insert  into `t_tourism`(`id`,`title`,`type`,`city`,`days`,`basePrice`,`minNumber`,`maxNumber`,`description`,`del`) values (1,'软库网旅游',1,'深圳',80,2,1,888,'很好完',0),(2,'软库网旅游1',0,'深圳',80,2,1,888,'很好完',0),(3,'软库网旅游2',1,'深圳',80,2,1,888,'很好完',0),(4,'软库网旅游3',1,'深圳',80,2,1,888,'很好完',0),(5,'软库网旅游3',0,'深圳',80,2,1,888,'很好完',0);
 
 /*Table structure for table `t_tourismimages` */
 
@@ -151,11 +174,11 @@ CREATE TABLE `t_tourismimages` (
   PRIMARY KEY (`ID`),
   KEY `FK_image_tourism` (`tourism_id`),
   CONSTRAINT `FK_image_tourism` FOREIGN KEY (`tourism_id`) REFERENCES `t_tourism` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 /*Data for the table `t_tourismimages` */
 
-insert  into `t_tourismimages`(`ID`,`tourism_id`,`big`,`small`,`upload_time`,`last_modify`,`orders`,`del`) values (8,21,'upload/001.jpg','upload/001.jpg','2015-12-04 16:43:49','2015-12-04 00:00:00',1814650725,0),(9,21,'upload/002.jpg','upload/002.jpg','2015-12-04 16:43:49','2015-12-04 00:00:00',1814650860,0),(10,22,'upload/002.jpg','upload/002.jpg','2015-12-04 16:44:23','2015-12-04 00:00:00',1814685214,0),(11,22,'upload/004.jpg','upload/004.jpg','2015-12-04 16:44:24','2015-12-04 00:00:00',1814685372,0),(12,23,'upload/004.jpg','upload/004.jpg','2015-12-04 16:44:51','2015-12-04 00:00:00',1814712824,0),(13,24,'upload/001.jpg','upload/001.jpg','2015-12-04 16:47:59','2015-12-04 00:00:00',1814900759,0),(14,25,'upload/002.jpg','upload/002.jpg','2015-12-04 16:49:19','2015-12-04 00:00:00',1814980490,0),(15,26,'upload/004.jpg','upload/004.jpg','2015-12-04 16:49:40','2015-12-04 00:00:00',1815001540,0),(16,27,'upload/002.jpg','upload/002.jpg','2015-12-04 16:49:59','2015-12-04 00:00:00',1815020840,0),(17,28,'upload/004.jpg','upload/004.jpg','2015-12-04 16:50:17','2015-12-04 00:00:00',1815038291,0),(18,29,'upload/004.jpg','upload/004.jpg','2015-12-04 16:50:34','2015-12-04 00:00:00',1815055410,0),(19,51,'upload/002.jpg','upload/002.jpg','2015-12-04 16:54:16','2015-12-04 00:00:00',1815278120,0);
+insert  into `t_tourismimages`(`ID`,`tourism_id`,`big`,`small`,`upload_time`,`last_modify`,`orders`,`del`) values (1,1,'upload/001.jpg','upload/001.jpg','2015-12-09 16:08:37','2015-12-09 00:00:00',-2050428494,0),(2,1,'upload/002.jpg','upload/002.jpg','2015-12-09 16:08:37','2015-12-09 00:00:00',-2050428434,0),(3,2,'upload/002.jpg','upload/002.jpg','2015-12-09 16:08:57','2015-12-09 00:00:00',-2050408191,0),(4,2,'upload/003.jpg','upload/003.jpg','2015-12-09 16:08:57','2015-12-09 00:00:00',-2050408139,0),(5,3,'upload/003.jpg','upload/003.jpg','2015-12-09 16:09:19','2015-12-09 00:00:00',-2050386144,0),(6,3,'upload/001.jpg','upload/001.jpg','2015-12-09 16:09:20','2015-12-09 00:00:00',-2050385982,0),(7,4,'upload/003.jpg','upload/003.jpg','2015-12-09 16:09:47','2015-12-09 00:00:00',-2050358870,0),(8,4,'upload/002.jpg','upload/002.jpg','2015-12-09 16:09:47','2015-12-09 00:00:00',-2050358800,0),(9,5,'upload/002.jpg','upload/002.jpg','2015-12-09 16:10:13','2015-12-09 00:00:00',-2050332395,0),(10,5,'upload/004.jpg','upload/004.jpg','2015-12-09 16:10:13','2015-12-09 00:00:00',-2050332325,0);
 
 /*Table structure for table `t_tourismrule` */
 
@@ -175,11 +198,11 @@ CREATE TABLE `t_tourismrule` (
   PRIMARY KEY (`ID`),
   KEY `FK_tourism_tourismRule` (`tourism_id`),
   CONSTRAINT `FK_tourism_tourismRule` FOREIGN KEY (`tourism_id`) REFERENCES `t_tourism` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 /*Data for the table `t_tourismrule` */
 
-insert  into `t_tourismrule`(`ID`,`tourism_id`,`deadline`,`tourismTime`,`price`,`discount`,`status`,`remark`,`modifyTime`,`del`) values (22,21,'2016-05-10 00:00:00','2016-05-13 00:00:00',555.00,'666',0,'好玩','2015-12-04 16:43:38',0),(23,22,'2016-05-10 00:00:00','2016-05-13 00:00:00',555.00,'666',0,'好玩','2015-12-04 16:44:12',0),(24,23,'2016-05-10 00:00:00','2016-05-13 00:00:00',25.00,'666',0,'好玩','2015-12-04 16:44:44',0),(25,24,'2016-05-10 00:00:00','2016-05-13 00:00:00',555.00,'666',0,'好玩','2015-12-04 16:47:52',0),(26,25,'2016-05-10 00:00:00','2016-05-13 00:00:00',555.00,'666',0,'好玩','2015-12-04 16:49:15',0),(27,26,'2016-05-10 00:00:00','2016-05-13 00:00:00',555.00,'666',0,'好玩','2015-12-04 16:49:34',0),(28,27,'2016-05-10 00:00:00','2016-05-13 00:00:00',555.00,'666',0,'好玩','2015-12-04 16:49:55',0),(29,28,'2016-05-10 00:00:00','2016-05-13 00:00:00',555.00,'666',0,'好玩','2015-12-04 16:50:13',0),(30,29,'2016-05-10 00:00:00','2016-05-13 00:00:00',555.00,'666',0,'好玩','2015-12-04 16:50:30',0),(31,51,'2016-05-10 00:00:00','2016-05-13 00:00:00',555.00,'666',0,'好玩','2015-12-04 16:54:08',0);
+insert  into `t_tourismrule`(`ID`,`tourism_id`,`deadline`,`tourismTime`,`price`,`discount`,`status`,`remark`,`modifyTime`,`del`) values (1,1,'2016-05-10 00:00:00','2016-05-13 00:00:00',555.00,'666',0,'好玩','2015-12-09 16:08:29',0),(2,2,'2016-05-10 00:00:00','2016-05-13 00:00:00',555.00,'666',0,'好玩','2015-12-09 16:08:50',0),(3,3,'2016-05-10 00:00:00','2016-05-13 00:00:00',555.00,'666',0,'好玩','2015-12-09 16:09:13',0),(4,4,'2016-05-10 00:00:00','2016-05-13 00:00:00',555.00,'666',0,'好玩','2015-12-09 16:09:38',0),(5,5,'2016-05-10 00:00:00','2016-05-13 00:00:00',25.00,'666',0,'好玩','2015-12-09 16:10:06',0);
 
 /*Table structure for table `t_user` */
 
@@ -199,7 +222,7 @@ CREATE TABLE `t_user` (
 
 /*Data for the table `t_user` */
 
-insert  into `t_user`(`id`,`name`,`password`,`phone`,`email`,`type`,`status`,`del`) values (1,'admin','admin','11','admin@qq.com',1,0,0),(2,'a','a','11','a',0,0,0);
+insert  into `t_user`(`id`,`name`,`password`,`phone`,`email`,`type`,`status`,`del`) values (1,'huangyueran','huangyueran','111','111@qq.com',0,0,0),(2,'admin','admin','222','admin@qq.com',1,0,0);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
